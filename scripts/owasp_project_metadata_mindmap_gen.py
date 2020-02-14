f46bde40d81cd35fc3448e93653843ef0405eab9
spyros <morfeas3000@gmail.com>
2020-02-14 16:13:36 +0000

    addresses comments, improves script a tiny bit

--- scripts/owasp_project_metadata_mindmap_gen.py
+++ scripts/owasp_project_metadata_mindmap_gen.py
@@ -6,25 +6,24 @@ import sys
 import base64
 import time
 from collections import namedtuple
-from pprint import pprint 
+from pprint import pprint
 from random import randint
 from github3 import GitHub
 from urllib.parse import urlparse
 
 graph = pydot.Dot(graph_type="graph", rankdir="UD")
- 
+
 def add(graph, root, children):
     for child in children:
         edge = pydot.Edge(root,child)
         graph.add_edge(edge)
 
-def fetch_metadata(orgname:str,token:str):
+def enhance_metadata(reponame:str, orgname:str,token:str,sdlc_steps:{}) ->{}:
     """ searches repos in the org defined
         finds the metadata files and fetches them
         returns metadata files groupped by sdlc step
     """
-    data = gather_metadata(orgname,token)
-    sdlc_steps = {}
+    data = gather_metadata(reponame=reponame,orgname=orgname,token=token)
     for dat in data:
         for step in dat.get('sdlc', None):
             sdlc_steps[step].append(dat)
@@ -37,15 +36,17 @@ def add_file_to_graph(filename:str):
     """
     json.load(filename)
 
-def gather_metadata(orgname:str,token:str) ->[]:
+def gather_metadata(reponame:str, orgname:str,token:str) ->[]:
     """Builds the repo object for every repo in the org
     :param options the settings object
     :returns list of type Helper containing repo objects with repo url, clone url and options for each object
     """
     result = []
     connection = GitHub(token=token)
-    metadata_files = connection.search_code("org:owasp filename:*.json path:/")
-    for f in metadata_files:
+    org_metadata_files = connection.search_code("org:%s filename:*.json path:/"%orgname)
+    repo_metadata_file = connection.search_code("repo:%s, filename:*.json path:/"%reponame)
+    #TODO(@northdpole): this can be done using *_metadata_file.extend()
+    for f in org_metadata_files:
         time.sleep(1)
         content = requests.get(f.url)
         if content.status_code == 200:
@@ -54,13 +55,33 @@ def gather_metadata(orgname:str,token:str) ->[]:
             result.append(metadata)
         else:
             print("ERROR response code: %s"%content.status_code)
-            
+    for f in repo_metadata_file:
+        time.sleep(1)
+        content = requests.get(f.url)
+        if content.status_code == 200:
+            resp = json.loads(content.text)
+            metadata = json.loads(base64.b64decode(resp['content']))
+            result.append(metadata)
+        else:
+            print("ERROR response code: %s"%content.status_code)
     return result
 
 mindmap = {"SDLC":{"Planning":{},"Analysis":{},"Design":{},"Implementation":{},"Maintenance":{},"Strategy":{},"Culture":{}}}
 
+orgs = {"owasp":"owasp", "zap":"zap"} # this should eventually be a json or some other easily parsable file mapping repos to projects
+repos = {"":""}
+metadata = {}
+for human_org_name,github_org_name in orgs:
+    metadata = enhance_metadata(orgname=github_org_name,token=sys.argv[1])
 
-metadata = fetch_metadata("owasp",sys.argv[1])
+for human_repo_name,github_repo_name in repos:
+    metadata = enhance_metadata(reponame=github_repo_name,token=sys.argv[1],metadata=metadata)
+
+# TODO(@northdpole): validate metadata can actually be appended, perhaps it makes sense to see how this would look like with an example org and repos :-)
+# TODO (@northdpole): make the expected values in info.json an object with ENUMS for fields
+# TODO (@northdpole): make this script not burn the eyes of everyone who looks at it.
+
+
 
 for key,value in mindmap.items():
     add(graph, key,value.keys())
@@ -69,4 +90,3 @@ for key,value in mindmap.items():
             add(graph, step,items)
 graph.write_png("map.png")
 os.stat("map.png")
-
